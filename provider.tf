1541dec2c5c5031019ce5d3ae366e90fbaa526f2
David Hill <dhill@resultant.com>
2024-03-03 13:17:23 -0500

    updates

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    harvester = {
+      source  = "harvester/harvester"
+      version = "0.6.4"
+    }
+  }
+}
+
+provider "harvester" {
+}
